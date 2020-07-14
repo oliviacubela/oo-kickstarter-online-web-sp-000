701b12767ddae0aaf2f2f2f86c3f90eab12c3005
Olivia Cubela <oliviacubela@gmail.com>
2020-07-14 02:58:54 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+  attr_reader :backed_projects, :name, :title
+
+  def initialize(name)
+    @backed_projects = []
+    @name = name
+    @title = title
+  end
+
+  def back_project(project)
+    project = Project.new(title)
+    @backed_projects << project
+  end
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_reader :backers, :title
+
+  def initialize(title)
+    @backers = []
+    @title = title
+  end
+
+  def add_backer(backer)
+    backer = Backer.new(name)
+    @backers << backer
+  end
+
+end
